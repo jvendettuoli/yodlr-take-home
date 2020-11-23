87ab986fd405a29c3321ea926895bc817c5e01f1
Justin Vendettuoli <jvendettuoli@gmail.com>
2020-11-23 15:33:37 -0800

    basic styling on AdminPage

--- yodlr-take-home-frontend/src/AdminPage.js
+++ yodlr-take-home-frontend/src/AdminPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { DataGrid } from '@material-ui/data-grid';
@@ -9,7 +10,19 @@ import YodlrApi from './YodlrApi';
 import Register from './RegisterPage';
 import UsersTable from './UsersTable';
 
+const useStyles = makeStyles({
+	title : {
+		backgroundColor : '#004285',
+		color           : 'white'
+	},
+	table : {
+		margin : 20
+	}
+});
+
 function AdminPage() {
+	const classes = useStyles();
+
 	const [ users, setUsers ] = useState();
 	useEffect(() => {
 		async function getUsers() {
@@ -33,10 +46,19 @@ function AdminPage() {
 	};
 
 	return (
-		<Grid item className="AdminPage">
-			<Typography>Yodlr Admin</Typography>
-			<Grid item>
-				<Typography>Users</Typography>
+		<Grid container className="AdminPage">
+			<Grid item xs={12}>
+				<Typography
+					className={classes.title}
+					variant="h2"
+					align="center"
+					gutterBottom
+				>
+					Admin Dashboard
+				</Typography>
+			</Grid>
+			<Grid item xs={12} className={classes.table}>
+				<Typography variant="h4">Users</Typography>
 				{usersLoaded()}
 			</Grid>
 		</Grid>
