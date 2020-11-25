bb17b5c8e9bb525a5cc8062285045313e2a8f37f
Justin Vendettuoli <jvendettuoli@gmail.com>
2020-11-25 13:20:07 -0800

    added user page link to admin users table

--- yodlr-take-home-frontend/src/UsersTable.js
+++ yodlr-take-home-frontend/src/UsersTable.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
 import { DataGrid } from '@material-ui/data-grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 function UsersTable({ users, setSelectedRows }) {
 	console.debug('UserTable Component - Start');
 
+	users = users.map((user) => ({ ...user, link: `/users/${user.id}` }));
+
+	console.log('USRS', users);
 	const columns = [
 		{ field: 'id', headerName: 'ID', width: 70 },
 		{ field: 'firstName', headerName: 'First Name', width: 150 },
 		{ field: 'lastName', headerName: 'Last Name', width: 150 },
 		{ field: 'email', headerName: 'Email', width: 200 },
-		{ field: 'state', headerName: 'State', width: 120 }
+		{ field: 'state', headerName: 'State', width: 120 },
+		{
+			field      : 'link',
+			headerName : 'User Page',
+			width      : 150,
+			renderCell : (params) => (
+				<Link
+					style={{ zIndex: 1 }}
+					component={RouterLink}
+					to={params.value}
+				>
+					Link
+				</Link>
+			)
+		}
 	];
 
 	const handleChange = (evt) => {
